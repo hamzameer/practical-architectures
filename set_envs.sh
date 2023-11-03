6c5546973250536a4e23e0169b55e56d90ae87c4
Michael Holcomb <michael.holcomb@utsouthwestern.edu>
2023-11-03 09:53:40 -0500

    Permissions won't let us write lock/share these files

--- set_envs.sh
+++ set_envs.sh
@@ -4,5 +4,4 @@
 # Run with `source set_envs.sh`
 
 export HUGGINGFACE_HUB_CACHE=/project/nanocourse/PracArchDL23/shared/Day2Afternoon/cache/hub
-export HF_DATASETS_CACHE=/project/nanocourse/PracArchDL23/shared/Day2Afternoon/cache/datasets
 export EXAMPLE_FILE=/project/nanocourse/PracArchDL23/shared/Day2Afternoon/data/test.mp4
